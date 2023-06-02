1b07c45c6b71db620478b5e43082e3a30fa28cac
Andrew <malinaenglishclub@gmail.com>
2023-06-02 22:31:34 +0300

    build

--- src/index.js
+++ src/index.js
@@ -3,39 +3,38 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from './store/store'
-import Header from './Components/Header/Header'
+import store from "./store/store";
+import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import TaskDetails from "./Components/TaskDetails/TaskDetails";
 import Main from "./Components/Main/Main";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <>
-          <Header />
-          <Main/>
-          <Footer />
-        </>
-      ),
-    },
-    {
-      path: "/tasks/:taskId",
-      element: (
-        <>
-          <Header />
-          <TaskDetails />
-          <Footer />
-        </>
-      ),
-    }
-  ]);
+  {
+    path: "/",
+    element: (
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: "/tasks/:taskId",
+    element: (
+      <>
+        <Header />
+        <TaskDetails />
+        <Footer />
+      </>
+    ),
+  },
+]);
 
-
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(
-      <Provider store={store}>
-          <RouterProvider router={router} />
-      </Provider>
-  );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
